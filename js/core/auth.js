91fdcbfc5f74f32efa58b09829947d4d7bd032df
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 12:25:35 +0300

    Update auth.js

--- js/core/auth.js
+++ js/core/auth.js
@@ -1,9 +1,9 @@
 function loadLogin(){
 
-document.getElementById("app").innerHTML=`
-<div style="height:100vh;display:flex;justify-content:center;align-items:center">
+document.getElementById("app").innerHTML = `
+<div style="height:100vh;display:flex;justify-content:center;align-items:center;background:#0f172a">
 
-<div class="card" style="width:300px;text-align:center">
+<div style="background:white;padding:30px;border-radius:10px;width:300px;text-align:center;color:black">
 
 <h2>Hotel ERP Login</h2>
 
@@ -13,7 +13,8 @@ style="width:100%;padding:10px;margin:10px 0">
 <input id="password" type="password" placeholder="Password"
 style="width:100%;padding:10px;margin:10px 0">
 
-<button onclick="login()" style="background:#22c55e;color:white">
+<button onclick="login()"
+style="width:100%;padding:10px;background:#22c55e;color:white;border:none">
 Login
 </button>
 
@@ -24,33 +25,29 @@ Login
 
 function login(){
 
-let u=document.getElementById("username").value;
-let p=document.getElementById("password").value;
+let u = document.getElementById("username").value;
+let p = document.getElementById("password").value;
 
-if(u==="admin" && p==="1234"){
+if(u === "admin" && p === "1234"){
 localStorage.setItem("login","ok");
 loadDashboard();
 }else{
 alert("Wrong login");
 }
+
 }
 
 function checkLogin(){
 
-if(localStorage.getItem("login")==="ok"){
-    try{
-        loadDashboard();
-    }catch(e){
-        localStorage.clear();
-        loadLogin();
-    }
+if(localStorage.getItem("login") === "ok"){
+loadDashboard();
 }else{
-    loadLogin();
+loadLogin();
 }
 
 }
 
 function logout(){
-localStorage.clear();
+localStorage.removeItem("login");
 loadLogin();
 }
