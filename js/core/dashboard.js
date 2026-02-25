87576ecd346fe9030b32388cc6d718cbe52030d1
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 12:26:54 +0300

    Update dashboard.js

--- js/core/dashboard.js
+++ js/core/dashboard.js
@@ -1,6 +1,6 @@
 function loadDashboard(){
 
-document.getElementById("app").innerHTML=`
+document.getElementById("app").innerHTML = `
 
 <div style="display:flex;height:100vh">
 
@@ -8,21 +8,24 @@ document.getElementById("app").innerHTML=`
 
 <h2>Hotel ERP</h2>
 
-<button onclick="loadRoomManagement()">Room Management</button>
-
-<button onclick="logout()" 
-style="background:red;color:white;margin-top:20px">
+<button onclick="loadHome()">Home</button>
+<button onclick="logout()" style="background:red;color:white;margin-top:20px">
 Logout
 </button>
 
 </div>
 
-<div id="dashboardContent" style="flex:1;padding:30px">
-<h2>Welcome Dashboard ✅</h2>
+<div id="dashboardContent" style="flex:1;padding:30px;background:#0f172a;color:white">
+<h2>Dashboard Working ✅</h2>
 </div>
 
 </div>
 
 `;
 
+}
+
+function loadHome(){
+document.getElementById("dashboardContent").innerHTML =
+"<h2>Welcome Back ✅</h2>";
 }
