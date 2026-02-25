6d776a84ba1e52061a25bf8065bf99351a56b26e
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 09:36:35 +0300

    Refactor storage functions for rooms and bills

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,7 +1,10 @@
-function getRooms() {
-    return JSON.parse(localStorage.getItem("rooms")) || [];
+let rooms = JSON.parse(localStorage.getItem("rooms") || "[]");
+let bills = JSON.parse(localStorage.getItem("bills") || "[]");
+
+function saveRooms(){
+    localStorage.setItem("rooms", JSON.stringify(rooms));
 }
 
-function saveRooms(rooms) {
-    localStorage.setItem("rooms", JSON.stringify(rooms));
+function saveBills(){
+    localStorage.setItem("bills", JSON.stringify(bills));
 }
