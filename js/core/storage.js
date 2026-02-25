fbb1393de535f475362facffbb4937b306fb938a
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 11:27:36 +0300

    Update storage.js

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,27 +1,13 @@
-let rooms=[];
-let menus=[];
-let bills=[];
-let bookings=[];
-let users=[];
-
-function loadDatabase(){
-
-rooms=JSON.parse(localStorage.getItem("rooms")||"[]");
-menus=JSON.parse(localStorage.getItem("menus")||"[]");
-bills=JSON.parse(localStorage.getItem("bills")||"[]");
-users=JSON.parse(localStorage.getItem("users")||"[]");
-bookings=JSON.parse(localStorage.getItem("bookings")||"[]");
-
-}
+let rooms = JSON.parse(localStorage.getItem("rooms")||"[]");
+let menus = JSON.parse(localStorage.getItem("menus")||"[]");
+let bills = JSON.parse(localStorage.getItem("bills")||"[]");
+let users = JSON.parse(localStorage.getItem("users")||"[]");
+let bookings = JSON.parse(localStorage.getItem("bookings")||"[]");
 
 function saveDatabase(){
-
 localStorage.setItem("rooms",JSON.stringify(rooms));
 localStorage.setItem("menus",JSON.stringify(menus));
 localStorage.setItem("bills",JSON.stringify(bills));
 localStorage.setItem("users",JSON.stringify(users));
 localStorage.setItem("bookings",JSON.stringify(bookings));
-
 }
-
-loadDatabase();
