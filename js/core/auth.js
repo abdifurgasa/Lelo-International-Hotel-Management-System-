684e994187956091a9fa50c2fef8773fa19ca8fd
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 10:05:58 +0300

    Enhance login form with role selection and styles

--- js/core/auth.js
+++ js/core/auth.js
@@ -4,16 +4,20 @@ document.getElementById("app").innerHTML=`
 <div style="height:100vh;display:flex;justify-content:center;align-items:center;
 background:linear-gradient(135deg,#6a11cb,#2575fc,#2ecc71)">
 
-<div style="background:white;padding:30px;border-radius:20px;width:300px;text-align:center">
+<div class="card" style="width:300px;text-align:center">
 
-<h2>🏨 Hotel Login</h2>
+<h2>🏨 Hotel ERP Pro</h2>
 
-<input id="username" placeholder="Username" style="width:100%;padding:10px;margin:10px 0">
+<input id="username" placeholder="Username" style="width:100%;padding:12px;margin:10px 0">
 
-<input id="password" type="password" placeholder="Password"
-style="width:100%;padding:10px;margin:10px 0">
+<input id="password" type="password" placeholder="Password" style="width:100%;padding:12px;margin:10px 0">
 
-<button onclick="login()" style="width:100%;padding:12px;background:#2ecc71;color:white;border:none;border-radius:10px">
+<select id="role" style="width:100%;padding:10px;margin-bottom:10px">
+<option value="admin">Admin</option>
+<option value="reception">Reception</option>
+</select>
+
+<button onclick="login()" style="width:100%;padding:12px;background:#22c55e;border:none;color:white;border-radius:10px">
 Login
 </button>
 
@@ -27,25 +31,27 @@ function login(){
 
 let u=document.getElementById("username").value;
 let p=document.getElementById("password").value;
+let role=document.getElementById("role").value;
 
 if(u==="admin" && p==="1234"){
-    localStorage.setItem("login","ok");
-    loadDashboard();
+localStorage.setItem("login","ok");
+localStorage.setItem("role",role);
+loadDashboard();
 }else{
-    alert("Wrong login");
+alert("Wrong login");
 }
 
 }
 
 function checkLogin(){
 if(localStorage.getItem("login")==="ok"){
-    loadDashboard();
+loadDashboard();
 }else{
-    loadLogin();
+loadLogin();
 }
 }
 
 function logout(){
-localStorage.removeItem("login");
+localStorage.clear();
 checkLogin();
 }
