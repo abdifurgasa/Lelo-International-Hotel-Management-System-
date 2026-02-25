6b2511002eda18f2394d52d5f35ccbb09cc1abd3
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 11:07:07 +0300

    Update login UI and functionality in auth.js

--- js/core/auth.js
+++ js/core/auth.js
@@ -4,20 +4,22 @@ document.getElementById("app").innerHTML=`
 <div style="height:100vh;display:flex;justify-content:center;align-items:center;
 background:linear-gradient(135deg,#6a11cb,#2575fc,#2ecc71)">
 
-<div class="card" style="width:300px;text-align:center">
+<div style="background:white;padding:30px;border-radius:20px;width:300px;text-align:center">
 
-<h2>🏨 Hotel ERP Pro</h2>
+<h2>🏨 Hotel ERP Login</h2>
 
-<input id="username" placeholder="Username" style="width:100%;padding:12px;margin:10px 0">
+<input id="username" placeholder="Username" style="width:100%;padding:10px;margin:10px 0">
 
-<input id="password" type="password" placeholder="Password" style="width:100%;padding:12px;margin:10px 0">
+<input id="password" type="password" placeholder="Password" style="width:100%;padding:10px;margin:10px 0">
 
 <select id="role" style="width:100%;padding:10px;margin-bottom:10px">
 <option value="admin">Admin</option>
+<option value="manager">Manager</option>
 <option value="reception">Reception</option>
+<option value="worker">Worker</option>
 </select>
 
-<button onclick="login()" style="width:100%;padding:12px;background:#22c55e;border:none;color:white;border-radius:10px">
+<button onclick="login()" style="width:100%;padding:12px;background:#22c55e;color:white;border:none;border-radius:10px">
 Login
 </button>
 
@@ -34,9 +36,12 @@ let p=document.getElementById("password").value;
 let role=document.getElementById("role").value;
 
 if(u==="admin" && p==="1234"){
+
 localStorage.setItem("login","ok");
 localStorage.setItem("role",role);
+
 loadDashboard();
+
 }else{
 alert("Wrong login");
 }
@@ -44,11 +49,13 @@ alert("Wrong login");
 }
 
 function checkLogin(){
+
 if(localStorage.getItem("login")==="ok"){
 loadDashboard();
 }else{
 loadLogin();
 }
+
 }
 
 function logout(){
