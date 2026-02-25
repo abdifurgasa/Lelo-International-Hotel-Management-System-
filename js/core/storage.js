f5f9cebaa25ed214c6d1c2a444ad45e07522bdca
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 11:25:45 +0300

    Add user data handling to storage functions

--- js/core/storage.js
+++ js/core/storage.js
@@ -2,23 +2,26 @@ let rooms=[];
 let menus=[];
 let bills=[];
 let bookings=[];
+let users=[];
 
-function loadStorage(){
+function loadDatabase(){
 
 rooms=JSON.parse(localStorage.getItem("rooms")||"[]");
 menus=JSON.parse(localStorage.getItem("menus")||"[]");
 bills=JSON.parse(localStorage.getItem("bills")||"[]");
+users=JSON.parse(localStorage.getItem("users")||"[]");
 bookings=JSON.parse(localStorage.getItem("bookings")||"[]");
 
 }
 
-function saveAll(){
+function saveDatabase(){
 
 localStorage.setItem("rooms",JSON.stringify(rooms));
 localStorage.setItem("menus",JSON.stringify(menus));
 localStorage.setItem("bills",JSON.stringify(bills));
+localStorage.setItem("users",JSON.stringify(users));
 localStorage.setItem("bookings",JSON.stringify(bookings));
 
 }
 
-loadStorage();
+loadDatabase();
