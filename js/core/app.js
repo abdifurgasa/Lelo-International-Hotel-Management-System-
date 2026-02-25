cac3be97bcb5a764decafe001ac2eddd46a57afd
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 12:11:59 +0300

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -1,3 +1,71 @@
 window.onload = function () {
     checkLogin();
 };
+
+let rooms = [];
+
+function loadRoomManagement(){
+
+let content = document.getElementById("dashboardContent");
+
+content.innerHTML = `
+
+<h2>Room Management</h2>
+
+<input id="roomType" placeholder="Room Type"
+style="padding:8px;margin:5px 0;width:200px">
+
+<input id="roomPrice" placeholder="Price"
+type="number"
+style="padding:8px;margin:5px 0;width:200px">
+
+<button onclick="addRoom()"
+style="padding:8px;background:#22c55e;color:white;border:none">
+Add Room
+</button>
+
+<h3>Room List</h3>
+<div id="roomList"></div>
+
+`;
+
+loadRooms();
+}
+
+function addRoom(){
+
+let type = document.getElementById("roomType").value;
+let price = document.getElementById("roomPrice").value;
+
+if(!type || !price){
+alert("Fill all fields");
+return;
+}
+
+rooms.push({
+id: Date.now(),
+type,
+price
+});
+
+loadRooms();
+}
+
+function loadRooms(){
+
+let list = document.getElementById("roomList");
+if(!list) return;
+
+list.innerHTML = "";
+
+rooms.forEach(r => {
+
+list.innerHTML += `
+<div style="background:#f1f1f1;color:black;padding:10px;margin:5px;width:200px">
+Room: ${r.type} <br>
+Price: ${r.price}
+</div>
+`;
+
+});
+}
