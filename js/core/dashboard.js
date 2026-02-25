894c5d8f0feeab0acaba9e5a03d9ab2e995042d0
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 07:10:06 +0300

    Update dashboard.js

--- js/core/dashboard.js
+++ js/core/dashboard.js
@@ -1,1 +1,8 @@
-
+function loadDashboard() {
+    document.getElementById("app").innerHTML = `
+        <div style="height:100vh;background:linear-gradient(135deg,green,purple);color:white;padding:20px;">
+            <h1>Welcome to Lelo International Hotel Dashboard</h1>
+            <button onclick="loadLogin()" style="padding:10px;background:white;color:black;border:none;">Logout</button>
+        </div>
+    `;
+}
