322a7495803e85a06a472555081f1839f2178372
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 11:05:44 +0300

    Refactor storage management for localStorage

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,17 +1,24 @@
-let rooms = JSON.parse(localStorage.getItem("rooms")||"[]");
-let bills = JSON.parse(localStorage.getItem("bills")||"[]");
-let menus = JSON.parse(localStorage.getItem("menus")||"[]");
+let rooms=[];
+let menus=[];
+let bills=[];
+let bookings=[];
 
-function saveAll(){
-localStorage.setItem("rooms",JSON.stringify(rooms));
-localStorage.setItem("bills",JSON.stringify(bills));
-localStorage.setItem("menus",JSON.stringify(menus));
+function loadStorage(){
+
+rooms=JSON.parse(localStorage.getItem("rooms")||"[]");
+menus=JSON.parse(localStorage.getItem("menus")||"[]");
+bills=JSON.parse(localStorage.getItem("bills")||"[]");
+bookings=JSON.parse(localStorage.getItem("bookings")||"[]");
+
 }
-let bookings = JSON.parse(localStorage.getItem("bookings")||"[]");
 
 function saveAll(){
+
 localStorage.setItem("rooms",JSON.stringify(rooms));
 localStorage.setItem("menus",JSON.stringify(menus));
 localStorage.setItem("bills",JSON.stringify(bills));
 localStorage.setItem("bookings",JSON.stringify(bookings));
+
 }
+
+loadStorage();
