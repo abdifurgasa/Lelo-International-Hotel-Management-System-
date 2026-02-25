3349f13cc87e56d320dad27017bd8a79bcb6e4a5
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 13:18:13 +0300

    Update dashboard.js

--- js/core/dashboard.js
+++ js/core/dashboard.js
@@ -5,24 +5,25 @@ document.getElementById("app").innerHTML = `
 <div style="display:flex;height:100vh">
 
 <!-- Sidebar -->
-<div style="width:250px;background:#020617;padding:20px;color:white">
+<div style="width:250px;background:#020617;padding:20px">
 
 <h2>Hotel ERP</h2>
 
 <button onclick="loadHome()">Home</button>
 <button onclick="loadRoomManagement()">Room Management</button>
 
-<button onclick="logout()" 
-style="background:red;color:white;margin-top:20px">
+<button onclick="logout()" style="background:red;color:white;margin-top:20px">
 Logout
 </button>
 
 </div>
 
 <!-- Content -->
-<div id="dashboardContent" 
+<div id="dashboardContent"
 style="flex:1;padding:30px;background:#0f172a;color:white">
-<h2>Welcome Dashboard ✅</h2>
+
+<h2>Dashboard ✅</h2>
+
 </div>
 
 </div>
