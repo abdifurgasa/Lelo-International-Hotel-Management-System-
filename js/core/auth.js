1e575e1f734dbba342a78402cb409b1de1f2425d
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-25 11:55:34 +0300

    Update auth.js

--- js/core/auth.js
+++ js/core/auth.js
@@ -1,32 +1,3 @@
-function loadLogin(){
-
-document.getElementById("app").innerHTML=`
-
-<div style="height:100vh;display:flex;justify-content:center;align-items:center;
-background:linear-gradient(135deg,#6a11cb,#2575fc,#2ecc71)">
-
-<div class="card" style="width:300px;text-align:center;background:white;color:black">
-
-<h2>🏨 Hotel ERP Login</h2>
-
-<input id="username" placeholder="Username"
-style="width:100%;padding:10px;margin:10px 0">
-
-<input id="password" type="password" placeholder="Password"
-style="width:100%;padding:10px;margin:10px 0">
-
-<button onclick="login()"
-style="background:#22c55e;color:white">
-Login
-</button>
-
-</div>
-</div>
-
-`;
-
-}
-
 function login(){
 
 let u=document.getElementById("username").value;
@@ -35,6 +6,7 @@ let p=document.getElementById("password").value;
 if(u==="admin" && p==="1234"){
 
 localStorage.setItem("login","ok");
+
 loadDashboard();
 
 }else{
@@ -55,5 +27,5 @@ loadLogin();
 
 function logout(){
 localStorage.clear();
-checkLogin();
+loadLogin();
 }
